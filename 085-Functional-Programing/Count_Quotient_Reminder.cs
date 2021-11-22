affc78b10a070f245564363dfeb79e80024fe7ee
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-22 15:15:53 +0530

    [Akash Added code for Quotient and reminder]

---
+++ 085-Functional-Programing/Count_Quotient_Reminder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional_Programing_085
+{
+    class Count_Quotient_Reminder
+    {
+        public static void solution()
+        {
+            int dividend, divisor;
+            Console.Write("Enter a number as dividend: ");
+            dividend = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number as divisor: ");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend = " + dividend + " Divisor = " + divisor);
+            Console.WriteLine("The quotient is: " + quotient);
+            Console.WriteLine("The remainder is: " + remainder);
+        }
+    }
+}
--- 085-Functional-Programing/Program.cs
+++ 085-Functional-Programing/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace _085_Functional_Programing
+namespace Functional_Programing_085
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to functional programing");
+            //Flip_Coin.Flip();
+            //Leap.DetermineLeapYear();
+            //Powerof2.PrintPower();
+            //Harmonic_PB4.HarmonicNum();
+            //Factor.primeFactors(20);
+            Count_Quotient_Reminder.solution();
         }
     }
 }
