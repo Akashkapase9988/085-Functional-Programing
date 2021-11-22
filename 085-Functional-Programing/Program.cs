2d5bef0ded74b32e956d9970edb647bc9a7aa0cd
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-22 13:58:58 +0530

    Add project files.

---
+++ 085-Functional-Programing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _085_Functional_Programing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to functional programing");
+        }
+    }
+}
