4389effb74f024ea5e44b1d65f761036dad703e2
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-22 15:10:07 +0530

    [Akash added code for prime factor ]

--- 085-Functional-Programing/Program.cs
+++ 085-Functional-Programing/Program.cs
@@ -7,6 +7,11 @@ namespace _085_Functional_Programing
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to functional programing");
+            //Flip_Coin.Flip();
+            //Leap.DetermineLeapYear();
+            //Powerof2.PrintPower();
+            //Harmonic_PB4.HarmonicNum();
+            Factor.primeFactors(20);
         }
     }
 }
