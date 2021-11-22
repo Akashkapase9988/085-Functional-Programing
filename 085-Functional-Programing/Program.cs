e228fec85e0d34999aabbbf52a0c4678eb0ac136
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-22 14:52:05 +0530

    [Akash added code for power of two ]

--- 085-Functional-Programing/Program.cs
+++ 085-Functional-Programing/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace _085_Functional_Programing
+namespace Functional_Programing_085
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to functional programing");
+            //Flip_Coin.Flip();
+            //Leap.DetermineLeapYear();
+            Powerof2.PrintPower();
         }
     }
 }
