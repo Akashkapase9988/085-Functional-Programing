0b176eff77dc7906cde42270f6fe929849169600
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-22 15:38:31 +0530

    [Akash added code for vowels]

--- 085-Functional-Programing/Program.cs
+++ 085-Functional-Programing/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace _085_Functional_Programing
+namespace Functional_Programing_085
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to functional programing");
+            Vowel_consonant.vowelconconat();
         }
     }
 }
---
+++ 085-Functional-Programing/Vowel_consonant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional_Programing_085
+{
+    class Vowel_consonant
+    {
+        public static void vowelconconat()
+        {
+
+            char ch;
+
+            Console.WriteLine("Enter any character: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+
+            // Condition for vowel checking
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+
+                Console.WriteLine(ch + " is Vowel.");
+
+            }
+            else
+            {
+                Console.WriteLine(ch + " is Consonant.");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
